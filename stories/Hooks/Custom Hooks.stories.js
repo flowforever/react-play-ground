4b62d94a4100785fadbb0d3613d408aad93f0360
trump.wang <trump.wang@ringcentral.com>
2018-11-28 14:30:20 +0800

    1. use yarn to install

--- stories/Hooks/Custom Hooks.stories.js
+++ stories/Hooks/Custom Hooks.stories.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createAddStory} from "../helper/index";
 import {StoryWrapper} from "../helper/StoryWrapper";
 
 const addStory = createAddStory(__filename);
 
+
+const fetchListFromFakeAPI = () => new Promise(resolve => {
+    console.log('Fetch user list');
+
+    setTimeout(() => resolve(
+        [
+            {name: 'trump wang', id: 1},
+            {name: 'ronnie o\'sullivan', id: 1},
+        ]
+    ), 500);
+});
+
+const useUserList = () => {
+    const [userList, setUserList] = useState([]);
+
+    useEffect(() => {
+        fetchListFromFakeAPI().then(setUserList);
+
+        return () => {
+
+        }
+    });
+
+    return [userList, setUserList];
+};
+
+function UserList() {
+    const [userList, setUserList] = useUserList();
+
+    return (
+        <div>
+            {userList.map(user => <UserItem user={user}/>)}
+        </div>
+    );
+}
+
+function UserItem({user}) {
+    return (
+        <div>
+            {user.id} - {user.name}
+        </div>
+    );
+}
+
 addStory(() => {
     return (
         <StoryWrapper>
-            Content
+            <UserList />
         </StoryWrapper>
     );
 });

e7b8ab62d6b1646080a227907cd50c21edc8a906
trump.wang <trump.wang@ringcentral.com>
2018-11-28 15:15:48 +0800

    1. learn custom Hooks with effect check

--- stories/Hooks/Custom Hooks.stories.js
+++ stories/Hooks/Custom Hooks.stories.js
@@ -1,51 +1,57 @@
 import React, {useEffect, useState} from 'react';
 import {createAddStory} from "../helper/index";
 import {StoryWrapper} from "../helper/StoryWrapper";
+import {itemMargins, itemPaddings} from "../helper/styles";
 
 const addStory = createAddStory(__filename);
 
-
-const fetchListFromFakeAPI = () => new Promise(resolve => {
+const fetchListFromFakeAPI = (groupId) => new Promise(resolve => {
     console.log('Fetch user list');
+
+    const createItem = (obj) => ({...obj, groupId});
 
     setTimeout(() => resolve(
         [
-            {name: 'trump wang', id: 1},
-            {name: 'ronnie o\'sullivan', id: 1},
+            createItem({name: 'trump wang', id: 1}),
+            createItem({name: 'ronnie o\'sullivan', id: 1}),
         ]
     ), 500);
 });
 
-const useUserList = () => {
+const useUserList = (groupId) => {
     const [userList, setUserList] = useState([]);
 
     useEffect(() => {
-        fetchListFromFakeAPI().then(setUserList);
+        fetchListFromFakeAPI(groupId).then(setUserList);
 
         return () => {
-
+            console.log('I am unmount.')
         }
-    });
+    }, [groupId]);
 
     return [userList, setUserList];
 };
 
-function UserList() {
-    const [userList, setUserList] = useUserList();
+function UserList({groupId}) {
+    const [userList] = useUserList(groupId);
 
     return (
         <div>
-            {userList.map(user => <UserItem user={user}/>)}
+            {userList.map(user => <UserItem key={`${user.id}-${user.name}`} {...user} />)}
         </div>
     );
 }
 
-function UserItem({user}) {
-    return (
-        <div>
-            {user.id} - {user.name}
-        </div>
-    );
+class UserItem extends React.PureComponent {
+    render() {
+        const {id, name} = this.props;
+
+        return (
+            <div style={{...itemMargins, ...itemPaddings}}>
+                {id} - {name}
+            </div>
+        );
+    }
 }
 
 addStory(() => {
---
+++ stories/helper/styles.js
@@ -0,0 +1,2 @@
+export const itemMargins = {margin: 10};
+export const itemPaddings = {padding: 10};
